5d1f33c6ec8805c272dd432718e6d34c816b5956
Aditya kumar <98655260+164adityakumar@users.noreply.github.com>
2023-07-30 14:31:02 +0530

    all tests passed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,22 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
-function isAnagram(str1, str2) {
-
+function sort(str)
+{
+  //sort the string
+  return str.split('').sort().join('');
 }
 
+function isAnagram(str1, str2) {
+ str1=sort(str1.toLowerCase());
+ str2=sort(str2.toLowerCase());
+ if(str1 === str2){
+   return true;
+ }
+  else{
+    return false;
+  }
+}
+
+// console.log(isAnagram('spar', 'rasp')); // true
 module.exports = isAnagram;
